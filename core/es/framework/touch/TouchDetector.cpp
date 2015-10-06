abe1a36c8f1c919c8741254a0221701b30b67323
eaglesakura <eagle.sakura@gmail.com>
2015-10-06 16:41:32 +0900

    Detectorをダブルタップに対応

--- core/es/framework/touch/TouchDetector.cpp
+++ core/es/framework/touch/TouchDetector.cpp
@@ -1,3 +1,4 @@
+#include <es/memory/Buffer.hpp>
 #include "TouchDetector.h"
 #include "TouchListener.hpp"
 
@@ -5,7 +6,7 @@ namespace es {
 
 
 TouchDetector::TouchDetector() {
-
+    
 }
 
 void TouchDetector::addListener(const selection_ptr<TouchListener> &listener) {
@@ -15,7 +16,7 @@ void TouchDetector::addListener(const selection_ptr<TouchListener> &listener) {
 void TouchDetector::removeListener(const selection_ptr<TouchListener> &listener) {
     auto itr = listeners.begin();
     const auto end = listeners.end();
-
+    
     while (itr != end) {
         if ((*itr) == listener) {
             listeners.erase(itr);
@@ -34,7 +35,7 @@ std::shared_ptr<TouchPoint> TouchDetector::getTouchPointFromIndex(uint32_t index
     if (index < (int) points.size()) {
         return points[index];
     }
-
+    
     return sp<TouchPoint>();
 }
 
@@ -65,7 +66,7 @@ bool TouchDetector::isMultiDrag() const {
 
 vec2 TouchDetector::getMultiTapCenter() const {
     assert(isMultiTap());
-
+    
     const vec2 &beginA = points[0]->getBeginPos();
     const vec2 &beginB = points[1]->getBeginPos();
     return (beginA + beginB) / 2.0f;
@@ -73,70 +74,78 @@ vec2 TouchDetector::getMultiTapCenter() const {
 
 float TouchDetector::getMultiTapPointDistance() const {
     assert(isMultiTap());
-
+    
     const vec2 &currentA = points[0]->getCurrentPos();
     const vec2 &currentB = points[1]->getCurrentPos();
     const auto diff = (currentA - currentB);
     return glm::length(diff);
 }
 
-void TouchDetector::removeTouchPointFromId(const uint64_t id) {
-    auto itr = points.begin();
-    const auto end = points.end();
-    while (itr != end) {
-        if ((*itr)->getId() == id) {
-            points.erase(itr);
-            return;
-        }
-        ++itr;
-    }
-}
 
 void TouchDetector::onUpdateFrame(const float deltaTimeSec) {
-    auto itr = points.begin();
-    auto end = points.end();
-    while (itr != end) {
-        bool handle = false;
-        auto pt = (*itr);
+    std::vector<uint64_t> clearItems;
 
-        if ((!pt->isDraging()) /* ドラッグされていない */ &&
-            pt->isLongTap(longTapMs) /* 規定時間以上押下している */) {
-            for (auto &listener : listeners) {
-                handle = (handle || listener->onLongClick(this, *pt));
+    // ロングタップ認識
+    {
+        auto itr = points.begin();
+        const auto end = points.end();
+        while (itr != end) {
+            bool handle = false;
+            auto pt = (*itr);
+            
+            if ((!pt->isDraging()) /* ドラッグされていない */ &&
+                pt->isLongTap(longTapMs) /* 規定時間以上押下している */) {
+                for (auto &listener : listeners) {
+                    if (listener->onLongClick(this, *pt)) {
+                        handle = true;
+                    }
+                }
             }
-        }
-
-        if (handle) {
-            // ハンドリングされたら、これを削除
-            itr = points.erase(itr);
-            end = points.end();
-        } else {
+            
+            if (handle) {
+                // ハンドリングされたら削除対象として認識
+                clearItems.push_back((*itr)->getId());
+            }
             ++itr;
+        }
+    }
+
+    // ドロップフレームの掃除
+    for (auto &drop : dropPoints) {
+        if (((uint32_t) drop.releaseTime.end().milliSecond()) > continuationClickMs) {
+            clearItems.push_back(drop.point->getId());
+        }
+    }
+
+    // 削除対象をクリア
+    if (!clearItems.empty()) {
+        for (const uint64_t id : clearItems) {
+            onHandlePoint(id);
         }
     }
 }
 
 void TouchDetector::onTouchEvent(const TouchEvent &event) {
-
+    
     const uint64_t id = event.id; // タッチポイントのID
     const uint32_t type = event.action; // イベントタイプ
     const float dragDistance = 30;
     sp<TouchPoint> _point = getTouchPointFromId(id); // 対象となるIDを取得する
-
+    
     if (!_point) {
         // ポイントが見つからなかった
-
+        
         if (getTouchPoints() >= 2 || type != TouchEvent::Action_Down) {
             // 既に規定以上のポイントがタップされているから無視
             return;
         }
-
+        
         // 最初のポイントが既に動いてる？
         _point.reset(new TouchPoint(id, points.size(), event.position, dragDistance));
-
+        
         // ポイントを追加する
         points.push_back(_point);
-
+        
         if (isSingleTap()) {
             // シングルタップ開始
             for (auto &listener : listeners) {
@@ -146,7 +155,7 @@ void TouchDetector::onTouchEvent(const TouchEvent &event) {
             // ピンチの開始
             const vec2 center = getMultiTapCenter();
             pinchDistance = getMultiTapPointDistance();
-
+            
             for (auto &listener : listeners) {
                 listener->onPinchBegin(this, center);
             }
@@ -154,9 +163,9 @@ void TouchDetector::onTouchEvent(const TouchEvent &event) {
     } else {
         // イベントを更新する
         _point->onTouchEvent(event);
-
+        
         if (TouchEvent::Action_Down == type) {
-
+            
         } else if (TouchEvent::Action_Move == type) {
             if (isSingleTap()) {
                 if (_point->isDrag()) {
@@ -172,11 +181,11 @@ void TouchDetector::onTouchEvent(const TouchEvent &event) {
                     for (auto &listener : listeners) {
                         listener->onPinchScaling(this, now_distance / pinchDistance, center);
                     }
-
+                    
                     pinchDistance = now_distance;
                 }
             }
-
+            
         } else if (TouchEvent::Action_Up == type) {
             if (isSingleTap()) {
                 // シングルタップ
@@ -184,12 +193,56 @@ void TouchDetector::onTouchEvent(const TouchEvent &event) {
                     for (auto &listener : listeners) {
                         listener->onDragEnd(this, (*_point));
                     }
+
+                    // 強制的にハンドリング完了
+                    onHandlePoint(_point->getId());
                 } else {
-                    for (auto &listener : listeners) {
-                        listener->onClick(this, (*_point));
+
+                    // ダブルタップのチェックを行う
+                    for (auto &drop : dropPoints) {
+                        if (drop.point->getId() != _point->getId()) {
+                            continue;
+                        }
+
+                        ++drop.numTaps;
+
+                        bool handle = false;
+                        for (auto &listener : listeners) {
+                            if (listener->onContinuationClick(this, *_point, drop.numTaps)) {
+                                handle = true;
+                            }
+                        }
+
+                        if (handle) {
+                            onHandlePoint(_point->getId());
+                            return;
+                        }
+                    }
+
+
+                    // ダブルタップチェックを見つけられなかったら、シングル扱いを行う
+                    {
+                        bool handle = false;
+                        for (auto &listener : listeners) {
+                            if (listener->onClick(this, (*_point))) {
+                                handle = true;
+                            }
+                        }
+
+                        // 一旦データを削除
+                        onHandlePoint(_point->getId());
+
+                        if (!handle) {
+                            // ハンドリングが行われなかったので、ダブルタップのチェックに回す
+                            DropData data;
+                            data.numTaps = 1;
+                            data.point = _point;
+                            data.releaseTime.start();
+
+                            dropPoints.push_back(data);
+                        }
                     }
                 }
-                removeTouchPointFromId(event.id);
             } else {
                 const vec2 center = getMultiTapCenter();
                 for (auto &listener : listeners) {
@@ -203,4 +256,36 @@ void TouchDetector::onTouchEvent(const TouchEvent &event) {
     }
 }
 
+void TouchDetector::onHandlePoint(const uint64_t id) {
+    // タップポイントを排除する
+    {
+        auto itr = points.begin();
+        const auto end = points.end();
+        
+        while (itr != end) {
+            if ((*itr)->getId() == id) {
+                points.erase(itr);
+                break;
+            } else {
+                ++itr;
+            }
+        }
+    }
+    
+    // キャッシュポイントを排除する
+    {
+        auto itr = dropPoints.begin();
+        const auto end = dropPoints.end();
+        
+        while (itr != end) {
+            if (itr->point->getId() == id) {
+                dropPoints.erase(itr);
+                break;
+            } else {
+                ++itr;
+            }
+            
+        }
+    }
+}
 }
--- core/es/framework/touch/TouchDetector.h
+++ core/es/framework/touch/TouchDetector.h
@@ -44,10 +44,37 @@ public:
     void onTouchEvent(const TouchEvent &event);
 
 private:
+
+    /**
+     * タップ情報を完全に排除する
+     */
+    void onHandlePoint(const uint64_t id);
+
+    /**
+     * 削除前の一時退避データ
+     * ダブルタップの判定に利用する
+     */
+    struct DropData {
+        sp<TouchPoint> point;
+        /**
+         * タップしたカウント
+         */
+        int numTaps = 0;
+        /**
+         * 最後に指を離してからのカウンタ
+         */
+        NanoTimer releaseTime;
+    };
+
     /**
      *
      */
     std::vector<sp<TouchPoint> > points;
+
+    /**
+     * 削除対象データの一覧
+     */
+    std::vector<DropData> dropPoints;
 
     /**
      * 通知するリスナ
@@ -62,12 +89,12 @@ private:
     /**
      * ロングタップとして認識するミリ秒
      */
-    int32_t longTapMs = 400;
+    uint16_t longTapMs = 400;
 
     /**
-     * IDを指定してポイントを削除する
+     * 連続タップとして認識するミリ秒
      */
-    void removeTouchPointFromId(const uint64_t id);
+    uint16_t continuationClickMs = 600;
 
     /**
      * 1点タッチをしている
